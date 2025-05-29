64d57f25ffe3655b988c8412b36a84d1b7772596
Oshin0721 <ehdgk3210@naver.com>
2025-05-29 14:44:01 +0900

    first commit

---
+++ HW01/HW01.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int printArray(int arr[], int size) {
+	cout << "배열의 원소들: ";
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+	return 0;
+}
+
+int printArray2(int arr[], int size) {
+	cout << "\n배열의 원소들 합계 와 평균: ";
+	int tot = 0;
+	for (int i = 0; i < size; i++) {
+		tot += arr[i];
+	}
+	cout << tot << " " << tot/size <<endl;
+	
+	return 0;
+}
+
+int main() {
+	int arr[5];
+
+		cout << "첫번째 정수를 입력하세요" << endl;
+		cin >> arr[0];
+		
+		cout << "두번째 정수를 입력하세요" << endl;
+		cin >> arr[1];
+	
+		cout << "세번째 정수를 입력하세요" << endl;
+		cin >> arr[2];
+	
+		cout << "네번째 정수를 입력하세요" << endl;
+		cin >> arr[3];
+	
+		cout << "다섯번째 정수를 입력하세요" << endl;
+		cin >> arr[4];
+	
+		double total = arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
+		double average = total / 5;
+
+		cout << "합계 : " << total << endl;
+		cout << "평균 : " << average << endl;
+		printArray(arr, 5);
+		printArray2(arr, 5);
+		
+	return 0;
+}
