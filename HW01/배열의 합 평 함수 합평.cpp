8e5d4a36f18cda68bd541502e4312fa63000473c
Oshin0721 <ehdgk3210@naver.com>
2025-05-30 17:49:56 +0900

    secend commit

--- HW01/배열의 합 평 함수 합평.cpp
+++ HW01/배열의 합 평 함수 합평.cpp
@@ -21,29 +21,48 @@ int printArray2(int arr[], int size) {
 	return 0;
 }
 
+void insertionSort(int arr[], int size, bool ascending) {
+	for (int i = 1; i < size; i++) {
+		int key = arr[i];
+		int j = i - 1;
+
+		while (j >= 0 && ((ascending && arr[j] > key) || (!ascending && arr[j] < key))) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = key;
+	}
+}
+
 int main() {
 	int arr[5];
-
-		cout << "첫번째 정수를 입력하세요" << endl;
-		cin >> arr[0];
 		
-		cout << "두번째 정수를 입력하세요" << endl;
-		cin >> arr[1];
-	
-		cout << "세번째 정수를 입력하세요" << endl;
-		cin >> arr[2];
-	
-		cout << "네번째 정수를 입력하세요" << endl;
-		cin >> arr[3];
-	
-		cout << "다섯번째 정수를 입력하세요" << endl;
-		cin >> arr[4];
-	
+	cout << "5개의 정수를 입력하세요: " << endl;
+	for (int i = 0; i < 5; i++) {
+		cout << i + 1 << "번째 정수: ";
+		cin >> arr[i];
+	}
+		
 		double total = arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
 		double average = total / 5;
 
 		cout << "합계 : " << total << endl;
 		cout << "평균 : " << average << endl;
+		
+		int choice;
+		cout << "정렬 방법을 선택하세요 (1: 오름차순, 2: 내림차순): ";
+		cin >> choice;
+
+		if (choice == 1) {
+			insertionSort(arr, 5, true);
+			cout << "오름차순 삽입 정렬 완료." << endl;
+		} else if (choice == 2) {
+			insertionSort(arr, 5, false);
+			cout << "내림차순 삽입 정렬 완료." << endl;
+		} else {
+			cout << "잘못된 선택입니다. 정렬하지 않습니다." << endl;
+		}
+
 		printArray(arr, 5);
 		printArray2(arr, 5);
 		
